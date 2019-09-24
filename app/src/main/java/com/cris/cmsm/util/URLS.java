ae60855cea49f4497bcb94b92db4289046bd49fb
rangasanju <rangasanju@gmail.com>
2019-09-24 16:55:20 +0530

    added IRREGULAR_CREW = "master/getIrregularCrewSummary";

--- app/src/main/java/com/cris/cmsm/util/URLS.java
+++ app/src/main/java/com/cris/cmsm/util/URLS.java
@@ -95,4 +95,7 @@ public class URLS {
     public static final String SAVE_LI_GRADING = "master/saveLIgrading";
     public static final String SAVE_LI_COUNSELLING = "master/saveCrewConsFP";
     public static final String LICREWSTATUS="master/getLICrewMonitoredstatus";
+
+
+    public static final String IRREGULAR_CREW = "master/getIrregularCrewSummary";
 }
