d99c5584036834bb1ac572fa3ff5b9aa71bcc67e
rangasanju <rangasanju@gmail.com>
2019-12-30 13:37:24 +0530

    Removed spaces from Li Departure and Arrival UI And reduced remark rows from 150 to 70. Close #2

---
+++ app/src/main/java/com/cris/cmsm/models/LiInspectionRecord.java
@@ -0,0 +1,249 @@
+package com.cris.cmsm.models;
+
+public class LiInspectionRecord {
+
+    private String refNo;
+    private String lpId;
+    private String coLpId;
+    private String trailLocoNo;
+    private String trainType;
+    private String cab;
+    private String bpcNo;
+    private String bpcPercent;
+    private String bpcIssueStation;
+    private String bpcValid;
+    private String noOfCoachWagon;
+    private String coachWagonType;
+    private String commodity;
+    private String guardName;
+    private String grossTon;
+    private String emrStart;
+    private String emrEnd;
+    private String regenStart;
+    private String regenEnd;
+    private String speedMeter;
+    private String vcd;
+    private String horns;
+    private String sander;
+    private String lpWeakRemark;
+    private String coLpWeakRemark;
+    private String otherDuty;
+    private String dateTime;
+
+
+    public String getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(String refNo) {
+        this.refNo = refNo;
+    }
+
+    public String getLpId() {
+        return lpId;
+    }
+
+    public void setLpId(String lpId) {
+        this.lpId = lpId;
+    }
+
+    public String getCoLpId() {
+        return coLpId;
+    }
+
+    public void setCoLpId(String coLpId) {
+        this.coLpId = coLpId;
+    }
+
+    public String getTrailLocoNo() {
+        return trailLocoNo;
+    }
+
+    public void setTrailLocoNo(String trailLocoNo) {
+        this.trailLocoNo = trailLocoNo;
+    }
+
+    public String getTrainType() {
+        return trainType;
+    }
+
+    public void setTrainType(String trainType) {
+        this.trainType = trainType;
+    }
+
+    public String getCab() {
+        return cab;
+    }
+
+    public void setCab(String cab) {
+        this.cab = cab;
+    }
+
+    public String getBpcNo() {
+        return bpcNo;
+    }
+
+    public void setBpcNo(String bpcNo) {
+        this.bpcNo = bpcNo;
+    }
+
+    public String getBpcPercent() {
+        return bpcPercent;
+    }
+
+    public void setBpcPercent(String bpcPercent) {
+        this.bpcPercent = bpcPercent;
+    }
+
+    public String getBpcIssueStation() {
+        return bpcIssueStation;
+    }
+
+    public void setBpcIssueStation(String bpcIssueStation) {
+        this.bpcIssueStation = bpcIssueStation;
+    }
+
+    public String getBpcValid() {
+        return bpcValid;
+    }
+
+    public void setBpcValid(String bpcValid) {
+        this.bpcValid = bpcValid;
+    }
+
+    public String getNoOfCoachWagon() {
+        return noOfCoachWagon;
+    }
+
+    public void setNoOfCoachWagon(String noOfCoachWagon) {
+        this.noOfCoachWagon = noOfCoachWagon;
+    }
+
+    public String getCoachWagonType() {
+        return coachWagonType;
+    }
+
+    public void setCoachWagonType(String coachWagonType) {
+        this.coachWagonType = coachWagonType;
+    }
+
+    public String getCommodity() {
+        return commodity;
+    }
+
+    public void setCommodity(String commodity) {
+        this.commodity = commodity;
+    }
+
+    public String getGuardName() {
+        return guardName;
+    }
+
+    public void setGuardName(String guardName) {
+        this.guardName = guardName;
+    }
+
+    public String getGrossTon() {
+        return grossTon;
+    }
+
+    public void setGrossTon(String grossTon) {
+        this.grossTon = grossTon;
+    }
+
+    public String getEmrStart() {
+        return emrStart;
+    }
+
+    public void setEmrStart(String emrStart) {
+        this.emrStart = emrStart;
+    }
+
+    public String getEmrEnd() {
+        return emrEnd;
+    }
+
+    public void setEmrEnd(String emrEnd) {
+        this.emrEnd = emrEnd;
+    }
+
+    public String getRegenStart() {
+        return regenStart;
+    }
+
+    public void setRegenStart(String regenStart) {
+        this.regenStart = regenStart;
+    }
+
+    public String getRegenEnd() {
+        return regenEnd;
+    }
+
+    public void setRegenEnd(String regenEnd) {
+        this.regenEnd = regenEnd;
+    }
+
+    public String getSpeedMeter() {
+        return speedMeter;
+    }
+
+    public void setSpeedMeter(String speedMeter) {
+        this.speedMeter = speedMeter;
+    }
+
+    public String getVcd() {
+        return vcd;
+    }
+
+    public void setVcd(String vcd) {
+        this.vcd = vcd;
+    }
+
+    public String getHorns() {
+        return horns;
+    }
+
+    public void setHorns(String horns) {
+        this.horns = horns;
+    }
+
+    public String getSander() {
+        return sander;
+    }
+
+    public void setSander(String sander) {
+        this.sander = sander;
+    }
+
+    public String getLpWeakRemark() {
+        return lpWeakRemark;
+    }
+
+    public void setLpWeakRemark(String lpWeakRemark) {
+        this.lpWeakRemark = lpWeakRemark;
+    }
+
+    public String getCoLpWeakRemark() {
+        return coLpWeakRemark;
+    }
+
+    public void setCoLpWeakRemark(String coLpWeakRemark) {
+        this.coLpWeakRemark = coLpWeakRemark;
+    }
+
+    public String getOtherDuty() {
+        return otherDuty;
+    }
+
+    public void setOtherDuty(String otherDuty) {
+        this.otherDuty = otherDuty;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+}
